f92baddd1f2ac46a7012883586f050953cdab49c
Makaziwe0203 <ntsenyanamakaziwe@gmail.com>
2025-06-19 03:24:21 +0200

    Lesson5:  Adding Variables.

--- python.py
+++ python.py
@@ -10,3 +10,14 @@ print(type(num2))
 
 # int num =3
 # float num =2
+
+#Adding Variables
+
+#Valid Variable
+my_variable = 10
+total_count = 0
+user = 'John'
+
+#Invalid variable
+second_variable = 10
+user_name = 20
